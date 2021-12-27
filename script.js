62c71f8caf72b4911f10ff3aa1c931b580028d5c
ermayankrajput <er.mayankrajput95@gmail.com>
2021-12-27 17:02:29 +0530

    Demo complete

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //by default menu is set from css
+    //Use local storage to get user preferences for menu, but on resize we should remove open wrapper
+    $(".toggle, .open-wrapper").click(function(){
+        console.log($(document).width())
+        var width = $(document).width()
+        if(width < 1500){
+            $(".open-wrapper").toggle();
+        }
+        $(".sidenav-lg").toggle();
+        $(".sidenav-sm").toggle();
+        
+    });
+    $(window).on('resize', function(){
+        var win = $(this);
+        $(".open-wrapper").hide();
+        if (win.width() >= 1500) { 
+            $(".sidenav-lg").show();
+            $(".sidenav-sm").hide();
+        }else{
+            $(".sidenav-lg").hide();
+            $(".sidenav-sm").show();
+        }
+    });
+});
