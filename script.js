2f23abf9764488783d3b898f5d0fd611514590bd
ermayankrajput <er.mayankrajput95@gmail.com>
2021-12-27 18:11:56 +0530

    toggle function with menu type

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 $(document).ready(function(){
-    //by default menu is set from css
-    //Use local storage to get user preferences for menu, but on resize we should remove open wrapper
-    $(".toggle, .open-wrapper").click(function(){
-        console.log($(document).width())
-        var width = $(document).width()
-        if(width < 1500){
-            $(".open-wrapper").toggle();
-        }
-        if(width > 1500){
-            menuActivated();
-        }
-        $(".sidenav-lg").toggle();
-        $(".sidenav-sm").toggle();
-        
-        
-    });
+    // By default menu is set from css
+    // Use local storage to get user preferences for menu, but on resize we should remove open wrapper
+    
     $(window).on('resize', function(){
         var win = $(this);
         $(".open-wrapper").hide();
         if (win.width() >= 1500) { 
             $(".sidenav-lg").show();
             $(".sidenav-sm").hide();
+            $(".main").removeClass("small-menu-activated");
+            $(".top-menu-data").removeClass("small-menu-activated");
         }else{
             $(".sidenav-lg").hide();
             $(".sidenav-sm").show();
@@ -30,4 +19,36 @@ $(document).ready(function(){
         $(".main").toggleClass("small-menu-activated");
         $(".top-menu-data").toggleClass("small-menu-activated");
     }
+
+    
 });
+
+// Just for the demo, we do not need that much code, as we already have local storage setup
+function toggleMenu(menu){
+    var width = $(document).width()
+    switch(menu) {
+        case 'small':
+            $(".open-wrapper").hide();
+            $(".sidenav-lg").hide();
+            $(".sidenav-sm").show();
+            if(width > 1500){
+                $(".main").addClass("small-menu-activated");
+                $(".top-menu-data").addClass("small-menu-activated");
+            }
+            break;
+        case 'large':
+            if(width < 1500){
+                $(".open-wrapper").show();
+            }else{
+                $(".main").removeClass("small-menu-activated");
+                $(".top-menu-data").removeClass("small-menu-activated");
+            }
+            $(".sidenav-lg").show();
+            $(".sidenav-sm").hide();
+            break;
+        default:
+            $(".open-wrapper").hide();
+            $(".sidenav-lg").hide();
+            $(".sidenav-sm").show();
+    }
+}
